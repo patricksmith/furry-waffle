e852739fb2de95e98f65cd87bfb2ed36f2d4c945
Patrick Smith <pjs482@gmail.com>
2016-04-10 19:26:34 -0400

    Initial Commit

---
+++ randomizer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  randomizer
+//
+//  Created by Patrick Smith on 4/10/16.
+//  Copyright © 2016 PromptWorks. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
